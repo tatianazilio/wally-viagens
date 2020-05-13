9f55a4665329c4439f8f3ccf8ef39b387d9c60b2
Tatiana Zilio <tatianazilio@gmail.com>
2020-05-13 14:39:48 -0300

    Padronização, e criação da pasta Middlewares e Configs

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/indexRoute");
-var usersRouter = require("./routes/usuarioRoute");
-var pagamentoRouter = require("./routes/pagamentoRoute");
-var destinoRouter = require("./routes/destinoRoute");
-var pacoteRouter = require("./routes/pacoteRoute")
+const indexRouter = require("./routes/indexRoute");
+const usersRouter = require("./routes/usuarioRoute");
+const pagamentoRouter = require("./routes/pagamentoRoute");
+const destinoRouter = require("./routes/destinoRoute");
+const pacoteRouter = require("./routes/pacoteRoute")
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
