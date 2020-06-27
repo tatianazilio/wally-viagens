44f6e1b320d72a3fe132c90201e49a0cf775f05a
diogo-alves-gouveia <diagouveia@gmail.com>
2020-06-27 16:36:21 -0300

    Linkagem da página dos pacotes para página de pacamento

---
+++ controllers/pagamentoController.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+const config = require("../config/database");
+const bcrypt = require("bcrypt");
+const { Pacote, Ambiente, AmbientePacote, Atracao, AtracaoPacote, Destino, DestinoPacote, Origem, OrigemPacote } = require("../models");
+const moment = require('moment');
+require('dotenv').config();
+
+const pagamentoController = {
+
+    view: async (req, res) => {
+        try {
+            let pacote = await Pacote.findByPk(req.params.id);
+            console.log(pacote);
+            
+            return res.render('pagamento', { pacote });
+        } catch (error) {
+            console.log(error);
+            return res.render('error', {error});
+        }
+    }
+
+}
+
+module.exports = pagamentoController;
